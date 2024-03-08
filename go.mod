c79d3ba8156f37fb9ba55afdcdf753d5bd22f84c
akshay <ask029849@gmail.com>
2024-03-08 23:01:09 +0530

    slight changes in the cli

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/akshay0074700747/projectandCompany_management_cli-utility
 
 go 1.21.5
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -2,22 +2,67 @@ package main
 
 import (
 	"archive/zip"
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"io/fs"
+	"mime/multipart"
+	"net/http"
 	"os"
+	"os/user"
 	"path/filepath"
+	"strconv"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 type funcc func()
 
+type ConfigStore struct {
+	ProjectID  string     `json:"project_id"`
+	Progresses []Progress `json:"progresses"`
+	StartIndex int        `json:"start_index"`
+}
+
+type Progress struct {
+	Key         string `json:"key"`
+	Description string `json:"description"`
+	Filename    string `json:"file_name"`
+}
+
+type SetUserCredentials struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+// type RemoteProgress struct {
+// 	Email           string        `json:"email"`
+// 	ProjectID       string        `json:"project_id"`
+// 	ProgressWithZip []ProgressZip `json:"progress_with_zip"`
+// }
+
+// type ProgressZip struct {
+// 	Progress    int     `json:"progress"`
+// 	Key         string  `json:"key"`
+// 	Description string  `json:"description"`
+// 	Zip         os.File `json:"zip"`
+// }
+
 // stores all the tags and the tasks associated with the tag
 var (
-	initDir     = ".manager"
-	argMap      = make(map[string]funcc)
-	snapshotKey string
+	initDir             = ".manager"
+	argMap              = make(map[string]funcc)
+	snapshotKey         string
+	snapshotDescription string
+	projectID           string
+	projectConf         = "configs.json"
+	setEmail            string
+	setName             string
+	userCreds           = ".manager.json"
+	isStaged            string
 )
 
 func main() {
@@ -26,16 +71,29 @@ func main() {
 	// flag.Parse()
 
 	arguments := os.Args[1:]
-	fmt.Println(arguments)
 	for i, arg := range arguments {
 		if arg == "-key" && i < len(arguments)-1 {
 			snapshotKey = arguments[i+1]
+		}
+		if arg == "init" && i < len(arguments)-1 {
+			projectID = arguments[i+1]
 			break
+		}
+		if arg == "-desc" && i < len(arguments)-1 {
+			snapshotDescription = arguments[i+1]
+			break
+		}
+		if arg == "-email" && i < len(arguments)-1 {
+			setEmail = arguments[i+1]
+		}
+		if arg == "-name" && i < len(arguments)-1 {
+			setName = arguments[i+1]
+		}
+		if arg == "-stage" && i < len(arguments)-1 {
+			isStaged = "true"
 		}
 	}
 	flag.CommandLine.Parse(arguments)
-
-	fmt.Println("here is the key", snapshotKey)
 
 	set()
 
@@ -64,6 +122,12 @@ func set() {
 
 	//for creating an empty repository which will hold the snapshots
 	argMap["init"] = func() {
+
+		if projectID == "" {
+			fmt.Println("please provide the projectID as well...")
+			os.Exit(1)
+		}
+
 		// for getting the absolute path of the current working directory
 		currentDir, err := os.Getwd()
 		if err != nil {
@@ -80,6 +144,27 @@ func set() {
 			os.Exit(1)
 		}
 
+		if err = os.Chdir(initDir); err != nil {
+			fmt.Println("cannot make directory ", err)
+			os.Exit(1)
+		}
+
+		configFile, err := os.Create(projectConf)
+		if err != nil {
+			fmt.Println("cannot make conf file ", err)
+			os.Exit(1)
+		}
+		c := ConfigStore{
+			ProjectID: projectID,
+		}
+
+		res, err := json.Marshal(c)
+		if err != nil {
+			fmt.Println("cannot write to file ", err)
+			os.Exit(1)
+		}
+		configFile.Write(res)
+
 		fmt.Println("Successfully initialized...")
 	}
 
@@ -89,6 +174,13 @@ func set() {
 			fmt.Println("You should provide a key for identifying the snapshot")
 			os.Exit(1)
 		}
+
+		if snapshotDescription == "" {
+			fmt.Println("You should provide a description for the snapshot")
+			os.Exit(1)
+		}
+
+		zipName := uuid.New().String()
 
 		currDir, err := os.Getwd()
 		if err != nil {
@@ -119,8 +211,8 @@ func set() {
 			os.Exit(1)
 		}
 
-		zipfilename := fmt.Sprintf("%s.zip", snapshotKey)
-		zipfilename = filepath.Join(initDir, zipfilename)
+		zipfilenamee := fmt.Sprintf("%s.zip", zipName)
+		zipfilename := filepath.Join(initDir, zipfilenamee)
 
 		zipFile, err := os.Create(zipfilename)
 		if err != nil {
@@ -153,7 +245,7 @@ func set() {
 			var zipEntry io.Writer
 			if info.IsDir() {
 				//
-				zipEntry, err = zipWriter.CreateHeader(&zip.FileHeader{
+				_, err = zipWriter.CreateHeader(&zip.FileHeader{
 					Name:   relativePath + "/",
 					Method: zip.Deflate,
 				})
@@ -186,6 +278,221 @@ func set() {
 			return nil
 		})
 
+		c := Progress{
+			Key:         snapshotKey,
+			Description: snapshotDescription,
+			Filename:    zipfilenamee,
+		}
+
+		var cp ConfigStore
+
+		if err = os.Chdir(initDir); err != nil {
+			fmt.Println("there has been an error in going to the init dir file", err)
+			return
+		}
+
+		file, err := os.Open(projectConf)
+		if err != nil {
+			fmt.Println("there has been an error opening the config.json file", err)
+			return
+		}
+		defer file.Close()
+
+		if err := json.NewDecoder(file).Decode(&cp); err != nil {
+			fmt.Println("there has been an error decoding the config.json file", err)
+			return
+		}
+
+		cp.Progresses = append(cp.Progresses, c)
+
+		confFile, err := os.Create(projectConf)
+		if err != nil {
+			fmt.Println("there has been an error creating the config.json file", err)
+			return
+		}
+		defer confFile.Close()
+
+		if err = json.NewEncoder(confFile).Encode(cp); err != nil {
+			fmt.Println("there has been an error writing to the config.json file", err)
+			return
+		}
+	}
+
+	//set email and name of the user
+	argMap["set"] = func() {
+
+		if setEmail == "" {
+			fmt.Println("the email field cannot be empty")
+			return
+		}
+		if setName == "" {
+			fmt.Println("the name field cannot be empty")
+			return
+		}
+
+		c := SetUserCredentials{
+			Email: setEmail,
+			Name:  setName,
+		}
+
+		usr, err := user.Current()
+		if err != nil {
+			fmt.Println("failed to get the current user ", err)
+			return
+		}
+
+		if err = os.Chdir(usr.HomeDir); err != nil {
+			fmt.Println("failed to load the current user home dir ", err)
+			return
+		}
+
+		file, err := os.Create(userCreds)
+		if err != nil {
+			fmt.Println("failed to create a manager directory in the current user home dir ", err)
+			return
+		}
+
+		if err = json.NewEncoder(file).Encode(c); err != nil {
+			fmt.Println("failed to write to the manager directory in the current user home dir ", err)
+			return
+		}
+	}
+
+	argMap["push"] = func() {
+
+		currDir, err := os.Getwd()
+		if err != nil {
+			fmt.Println("failed to get the current working directory ", err)
+			return
+		}
+
+		usr, err := user.Current()
+		if err != nil {
+			fmt.Println("failed to get the current user ", err)
+			return
+		}
+
+		if err = os.Chdir(usr.HomeDir); err != nil {
+			fmt.Println("failed to switch to the current user directory", err)
+			return
+		}
+
+		credFile, err := os.Open(userCreds)
+		if err != nil {
+			fmt.Println("failed to get the creds from the current user directory or the run manager set with your email and name for initializing it ", err)
+			return
+		}
+
+		var c SetUserCredentials
+		if err = json.NewDecoder(credFile).Decode(&c); err != nil {
+			fmt.Println("failed to decode the creds from the current user directory ", err)
+			return
+		}
+
+		if err = os.Chdir(fmt.Sprintf("%s/.manager", currDir)); err != nil {
+			fmt.Println("failed to change to the manager dir if not initialized manager please initialize it using manager init ", err)
+			return
+		}
+
+		conf, err := os.Open(projectConf)
+		if err != nil {
+			fmt.Println("failed to get the conf file ", err)
+			return
+		}
+
+		var cc ConfigStore
+		if err = json.NewDecoder(conf).Decode(&cc); err != nil {
+			fmt.Println("failed to decode the conf file ", err)
+			return
+		}
+
+		if isStaged == "true" && snapshotKey == "" {
+			fmt.Println("the key cannot be empty... ", err)
+			return
+		}
+
+		var i int
+		var buf bytes.Buffer
+		writer := multipart.NewWriter(&buf)
+
+		writer.WriteField("email", c.Email)
+		writer.WriteField("project_id", cc.ProjectID)
+		writer.WriteField("isStaged", isStaged)
+		writer.WriteField("key", snapshotKey)
+
+		if cc.StartIndex == len(cc.Progresses) {
+			fmt.Println("no new commits ", err)
+			return
+		}
+
+		for i = cc.StartIndex; i < len(cc.Progresses); i++ {
+
+			zipFile, err := os.Open(cc.Progresses[i].Filename)
+			if err != nil {
+				fmt.Println("failed to open the zip file ", err)
+				return
+			}
+			defer zipFile.Close()
+
+			part, err := writer.CreateFormFile("files", cc.Progresses[i].Filename)
+			if err != nil {
+				fmt.Println("failed to create form file ", err)
+				return
+			}
+
+			_, err = io.Copy(part, zipFile)
+			if err != nil {
+				fmt.Println("failed to copy zip file contents to form file ", err)
+				return
+			}
+
+			writer.WriteField("keys", cc.Progresses[i].Key)
+			writer.WriteField("descriptions", cc.Progresses[i].Description)
+			writer.WriteField("progresses", strconv.Itoa(i+1))
+		}
+
+		err = writer.Close()
+		if err != nil {
+			fmt.Println("failed to close multipart writer ", err)
+			return
+		}
+
+		cc.StartIndex = i
+
+		filee, err := os.Create(projectConf)
+		if err != nil {
+			fmt.Println("failed to create the zip file ", err)
+			return
+		}
+
+		if err = json.NewEncoder(filee).Encode(cc); err != nil {
+			fmt.Println("failed to encode the zip file ", err)
+			return
+		}
+
+		req, err := http.NewRequest("POST", "http://localhost:50000/snapshots/push", &buf)
+		if err != nil {
+			fmt.Println("failed to create HTTP request:", err)
+			return
+		}
+
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Println("failed to send HTTP request:", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			fmt.Println("unexpected response status code:", resp.StatusCode)
+			return
+		}
+
+		fmt.Println(isStaged)
+
+		fmt.Println("pushing completed successfully...")
 	}
 
 }
